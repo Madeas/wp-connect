c9dd05f27e61b7dcae21b8aa592c15ff671bb25c
Andrej <an.sharapov90@gmail.com>
2018-02-26 12:47:16 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ( is_active_sidebar( 'custom-footer-widget' ) ) : ?>
-    <div id="footer-widget-area" class="chw-widget-area widget-area" role="complementary">
-	<?php dynamic_sidebar( 'custom-footer-widget' ); 
-?>
