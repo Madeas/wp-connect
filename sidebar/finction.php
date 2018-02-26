2ba9ff1acc64184fd933ffec0533cbb71838d468
Andrej <an.sharapov90@gmail.com>
2018-02-26 17:24:56 +0300

    finction.php

--- sidebar/finction.php
+++ sidebar/finction.php
@@ -3,7 +3,7 @@ function true_register_wp_sidebars() {
 	/* В боковой колонке - первый сайдбар */
 	register_sidebar(
 		array(
-			'id' => 'true_side', // уникальный id
+			'id' => 'sidebar', // уникальный id
 			'name' => 'Боковая колонка', // название сайдбара
 			'description' => 'Перетащите сюда виджеты, чтобы добавить их.', // описание
 			'before_widget' => '<div id="%1$s" class="side widget %2$s">', // по умолчанию виджеты выводятся <li>-списком
@@ -11,20 +11,6 @@ function true_register_wp_sidebars() {
 			'before_title' => '<h3 class="widget-title">', // по умолчанию заголовки виджетов в <h2>
 			'after_title' => '</h3>'
 		)
-	);
- 
-	/* В подвале - второй сайдбар */
-	register_sidebar(
-		array(
-			'id' => 'true_foot',
-			'name' => 'Футер',
-			'description' => 'Перетащите сюда виджеты, чтобы добавить их.',
-			'before_widget' => '<div id="%1$s" class="foot widget %2$s">',
-			'after_widget' => '</div>',
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>'
-		)
-	);
+	); 
 }
- 
 add_action( 'widgets_init', 'true_register_wp_sidebars' );
