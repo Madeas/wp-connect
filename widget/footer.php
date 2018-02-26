e1df266eb6c1f52c73bf187e61e122d10c1c2857
Andrej <an.sharapov90@gmail.com>
2018-02-26 13:02:43 +0300

    Update footer.php

--- widget/footer.php
+++ widget/footer.php
@@ -1,5 +1,4 @@
 <?php
 if ( is_active_sidebar( 'custom-footer-widget' ) ) : ?>
-    <div id="footer-widget-area" class="chw-widget-area widget-area" role="complementary">
-	<?php dynamic_sidebar( 'custom-footer-widget' ); 
-?>
+	<?php dynamic_sidebar( 'custom-footer-widget' ); ?>
+<?php endif; ?>
