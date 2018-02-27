192adb8ea48291882de67b97c28b24ca2c3240f0
Andrej <an.sharapov90@gmail.com>
2018-02-27 13:47:32 +0300

    functions.php

--- miniature/functions.php
+++ miniature/functions.php
@@ -2,4 +2,9 @@
 if ( function_exists( 'add_theme_support' ) ) { 
 	add_theme_support( 'post-thumbnails' ); 
 }
+
+if ( function_exists( 'add_theme_support' ) ) { 
+	add_theme_support( 'post-thumbnails' ); 
+	add_image_size( 'full-thumbnail', 650, 250, true ); // название, ширина, высота, жесткая обрезка
+}
 ?>
