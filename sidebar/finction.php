ed6bef5a09096ffffe4abc3044753e6e161d844f
Andrej <an.sharapov90@gmail.com>
2018-02-27 14:35:39 +0300

    finction.php

--- sidebar/finction.php
+++ sidebar/finction.php
@@ -1,16 +1,20 @@
 function true_register_wp_sidebars() {
  
 	/* В боковой колонке - первый сайдбар */
-	register_sidebar(
-		array(
-			'id' => 'sidebar', // уникальный id
-			'name' => 'Боковая колонка', // название сайдбара
-			'description' => 'Перетащите сюда виджеты, чтобы добавить их.', // описание
-			'before_widget' => '<div id="%1$s" class="side widget %2$s">', // по умолчанию виджеты выводятся <li>-списком
-			'after_widget' => '</div>',
-			'before_title' => '<h3 class="widget-title">', // по умолчанию заголовки виджетов в <h2>
-			'after_title' => '</h3>'
-		)
-	); 
-}
-add_action( 'widgets_init', 'true_register_wp_sidebars' );
+function right_sidebar() {
+ 
+	$args = array(
+		'id'            => 'sidebar-right',
+		'name'          => __( 'Правая колонка', 'striped' ),
+		'description'   => __( 'Перетащите сюда виджеты, чтобы добавить их', 'striped' ),
+		'class'         => 'striped-widget',
+		'before_title'  => '<h3 class="widgettitle">',
+		'after_title'   => '</h3>',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+	);
+	register_sidebar( $args );
+ 
+} 
+// Hook into the 'widgets_init' action
+add_action( 'widgets_init', 'right_sidebar' );
