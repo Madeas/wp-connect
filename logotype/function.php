c5b96176e0bef8622172eadf406f689b35fe6637
Andrej Sharapov <an.sharapov90@gmail.com>
2018-02-28 17:20:30 +0300

    function.php

---
+++ logotype/function.php
@@ -0,0 +1,27 @@
+
+// создаем логотип
+add_theme_support( 'custom-logo' );
+add_theme_support( 'custom-logo', array(
+	'height'      => 100,
+	'width'       => 400,
+	'flex-height' => true,
+	'flex-width'  => true,
+	'header-text' => array( 'site-title', 'site-description' ),
+) );
+function theme_prefix_setup() {
+	
+	add_theme_support( 'custom-logo', array(
+		'height'      => 100,
+		'width'       => 400,
+		'flex-width' => true,
+	) );
+
+}
+add_action( 'after_setup_theme', 'theme_prefix_setup' );
+function theme_prefix_the_custom_logo() {
+	
+	if ( function_exists( 'the_custom_logo' ) ) {
+		the_custom_logo();
+	}
+
+}
